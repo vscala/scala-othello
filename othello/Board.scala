6fb1e046dbfd2c998e37e1444fb777c6b878ed5d
Vincent Scala <vincent2000nc@gmail.com>
2024-07-18 18:07:16 -0400

    Improved naming and simplified bit-masks

--- othello/Board.scala
+++ othello/Board.scala
@@ -4,96 +4,82 @@ import scala.annotation.tailrec
 
 type Bitboard = Long
 object Bitboard {
-  val left: Bitboard = 0x0101010101010101L
-  val right: Bitboard = left << 7
-  val top: Bitboard = 0xFFL
-  val bottom: Bitboard = top << 56
-
-  val topLeft: Bitboard = top | left
-  val topRight: Bitboard = top | right
-  val bottomLeft: Bitboard = bottom | left
-  val bottomRight: Bitboard = bottom | right
-
-  val notLeft: Bitboard = ~left
-  val notRight: Bitboard = ~right
-  val notTop: Bitboard = ~top
-  val notBottom: Bitboard = ~bottom
-
-  val notTopLeft: Bitboard = ~topLeft
-  val notTopRight: Bitboard = ~topRight
-  val notBottomLeft: Bitboard = ~bottomLeft
-  val notBottomRight: Bitboard = ~bottomRight
+  object Mask {
+    val NotWest: Bitboard = 0xfefefefefefefefeL
+    val NotEast: Bitboard = 0x7f7f7f7f7f7f7f7fL
+    val NotNorth: Bitboard = 0xffffffffffffff00L
+    val NotSouth: Bitboard = 0x00ffffffffffffffL
+    val NotNorthWest: Bitboard = 0xfefefefefefefe00L
+    val NotNorthEast: Bitboard = 0x7f7f7f7f7f7f7f00L
+    val NotSouthWest: Bitboard = 0x00fefefefefefefeL
+    val NotSouthEast: Bitboard = 0x007f7f7f7f7f7f7fL
+  }
 }
-extension (bb: Bitboard) {
-  def dilateLeft: Bitboard = (bb & Bitboard.notLeft) >>> 1
-  def dilateRight: Bitboard = (bb & Bitboard.notRight) << 1
-  def dilateUp: Bitboard = (bb & Bitboard.notTop) >>> 8
-  def dilateDown: Bitboard = (bb & Bitboard.notBottom) << 8
-  def dilateLeftUp: Bitboard = (bb & Bitboard.notTopLeft) >>> 9
-  def dilateRightUp: Bitboard = (bb & Bitboard.notTopRight) >>> 7
-  def dilateLeftDown: Bitboard = (bb & Bitboard.notBottomLeft) << 7
-  def dilateRightDown: Bitboard = (bb & Bitboard.notBottomRight) << 9
+
+extension (bitboard: Bitboard) {
+  def dilateWest: Bitboard = (bitboard & Bitboard.Mask.NotWest) >>> 1
+  def dilateEast: Bitboard = (bitboard & Bitboard.Mask.NotEast) << 1
+  def dilateNorth: Bitboard = (bitboard & Bitboard.Mask.NotNorth) >>> 8
+  def dilateSouth: Bitboard = (bitboard & Bitboard.Mask.NotSouth) << 8
+  def dilateNorthWest: Bitboard = (bitboard & Bitboard.Mask.NotNorthWest) >>> 9
+  def dilateNorthEast: Bitboard = (bitboard & Bitboard.Mask.NotNorthEast) >>> 7
+  def dilateSouthWest: Bitboard = (bitboard & Bitboard.Mask.NotSouthWest) << 7
+  def dilateSouthEast: Bitboard = (bitboard & Bitboard.Mask.NotSouthEast) << 9
 
   def dilate: Bitboard = {
-    dilateLeft | dilateRight | dilateUp | dilateDown | dilateLeftUp | dilateRightUp | dilateLeftDown | dilateRightDown
+    dilateWest | dilateEast | dilateNorth | dilateSouth | dilateNorthWest | dilateNorthEast | dilateSouthWest | dilateSouthEast
   }
   def dilate(direction: Direction.Value): Bitboard = direction match {
-    case Direction.Up => dilateUp
-    case Direction.Down => dilateDown
-    case Direction.Left => dilateLeft
-    case Direction.Right => dilateRight
-    case Direction.UpLeft => dilateLeftUp
-    case Direction.UpRight => dilateRightUp
-    case Direction.DownLeft => dilateLeftDown
-    case Direction.DownRight => dilateRightDown
+    case Direction.North =>     dilateNorth
+    case Direction.South =>     dilateSouth
+    case Direction.West =>      dilateWest
+    case Direction.East =>      dilateEast
+    case Direction.NorthWest => dilateNorthWest
+    case Direction.NorthEast => dilateNorthEast
+    case Direction.SouthWest => dilateSouthWest
+    case Direction.SouthEast => dilateSouthEast
   }
 
-  def and(other: Bitboard): Bitboard = bb & other
-  def or(other: Bitboard): Bitboard = bb | other
+  def and(other: Bitboard): Bitboard = bitboard & other
+  def or(other: Bitboard):  Bitboard = bitboard | other
 
-  def count: Int = java.lang.Long.bitCount(bb)
+  def count: Int = java.lang.Long.bitCount(bitboard)
 
   def toMoveSeq: Seq[Move] = {
     (0 until 64).filter { index =>
-      (bb & (1L << index)) != 0
+      (bitboard & (1L << index)) != 0
     }.map(Move.apply)
   }
 
   def display: String = {
-    val rows = (0 until 8).map { row =>
-      val rowBits = 0xFFL << (row * 8)
-      val rowStr = (0 until 8).map { col =>
+    (0 until 8).map { row =>
+      (0 until 8).map { col =>
         val bit = 1L << (row * 8 + col)
-        if ((bb & bit) != 0) "X" else "."
-      }.mkString(" ")
-      s"$rowStr\n"
-    }
-    rows.mkString
+        if ((bitboard & bit) != 0) "X" else "."
+      }.mkString("", " ", "\n")
+    }.mkString
   }
 }
 
 case class Board(white: Bitboard, black: Bitboard)
+
 object Board {
   val start: Board = Board(
     white = 0x0000001008000000L,
     black = 0x0000000810000000L
   )
 }
+
 extension (board: Board) {
   def display: String = {
-    val white = board.white
-    val black = board.black
-    val rows = (0 until 8).map { row =>
-      val rowBits = 0xFFL << (row * 8)
-      val rowStr = (0 until 8).map { col =>
+    (0 until 8).map { row =>
+      (0 until 8).map { col =>
         val bit = 1L << (row * 8 + col)
-        if ((white & bit) != 0) "W"
-        else if ((black & bit) != 0) "B"
+        if ((board.white & bit) != 0) "W"
+        else if ((board.black & bit) != 0) "B"
         else "."
-      }.mkString(" ")
-      s"$rowStr\n"
-    }
-    rows.mkString
+      }.mkString("", " ", "\n")
+    }.mkString
   }
 
   def occupied: Bitboard = board.white | board.black
@@ -105,9 +91,7 @@ extension (board: Board) {
   }
   def getOtherBoard(boardColor: BoardColor.Value): Bitboard = getBoard(boardColor.other)
 
-  def isValidMove(boardColor: BoardColor.Value)(move: Move): Boolean = {
-    isValidMove(boardColor)(move.toBitboard)
-  }
+  def isValidMove(boardColor: BoardColor.Value)(move: Move): Boolean = isValidMove(boardColor)(move.toBitboard)
   def isValidMove(boardColor: BoardColor.Value)(move: Bitboard): Boolean = {
     val board = getBoard(boardColor)
     val otherBoard = getOtherBoard(boardColor)
@@ -167,28 +151,31 @@ object BoardColor {
 
 object Direction {
   sealed trait Value
-  case object Up extends Value
-  case object Down extends Value
-  case object Left extends Value
-  case object Right extends Value
-  case object UpLeft extends Value
-  case object UpRight extends Value
-  case object DownLeft extends Value
-  case object DownRight extends Value
-  val values: Set[Value] = Set(Up, Down, Left, Right, UpLeft, UpRight, DownLeft, DownRight)
+  case object North extends Value
+  case object South extends Value
+  case object West extends Value
+  case object East extends Value
+  case object NorthWest extends Value
+  case object NorthEast extends Value
+  case object SouthWest extends Value
+  case object SouthEast extends Value
+  val values: Set[Value] = Set(North, South, West, East, NorthWest, NorthEast, SouthWest, SouthEast)
 }
 
 case class Move(index: Int) {
   require(index >= 0 && index < 64)
 }
+
 extension (move: Move) {
   def toBitboard: Bitboard = 1L << move.index
 }
 
 case class BoardState(board: Board, currentPlayer: BoardColor.Value)
+
 object BoardState {
   val start: BoardState = BoardState(Board.start, BoardColor.Black)
 }
+
 extension (boardState: BoardState) {
   def playMove(move: Move): BoardState = BoardState(boardState.board.playMove(boardState.currentPlayer, move), boardState.currentPlayer.other)
   def skipMove: BoardState = BoardState(boardState.board, boardState.currentPlayer.other)
